3e377de4be3629726f9d17c1224484c76240b40f
Matthias Mossner <Matze.MPC@gmx.de>
2012-09-20 20:37:25 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<?php
+echo "<title>Heizung Mossner</title>";
+?>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-1">
+<META HTTP-EQUIV="Refresh" CONTENT="300">
+<link rel="stylesheet" href="heating.css">
+</head>
+
+<frameset rows="*" cols="220, *">
+    <frame name="ctrl" scrolling="auto"   marginwidth="2" marginheight="0" src="left.php"  noresize frameborder="0">
+
+    <frameset rows="78, *" cols="1*">
+         <frame name="header" scrolling="no"   marginwidth="10" marginheight="10" src="header.php" noresize frameborder="0">
+         <frame name="main" scrolling="auto" marginwidth="10" marginheight="10" src="main.php" noresize frameborder="0">
+      </frameset>
+</frameset>
+<noframes>
+   <body>
+   		<p>You need a browser that supports frame to view this page.</p>
+	</body>
+</noframes>
+</html>
+
