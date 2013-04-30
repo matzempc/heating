3393d96704d346d775ba07939abda5f7d02ff157
Matthias Mossner <Matze.MPC@gmx.de>
2013-04-30 22:00:20 +0200

    added reload time

--- main.php
+++ main.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
   <head>
+<META HTTP-EQUIV="Refresh" CONTENT="300">
 <link rel="stylesheet" href="heating.css">
 <title>Heizung</title>
   </head>
